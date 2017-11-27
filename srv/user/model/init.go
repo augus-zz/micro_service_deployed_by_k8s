bbbf584d11d24b9249c49691a15eca3a7892fe10
zouqilin <mr.zouqilin@gmail.com>
2017-11-27 17:47:37 +0800

    mysql env var for user service

--- srv/user/model/init.go
+++ srv/user/model/init.go
@@ -13,11 +13,13 @@ var (
 )
 
 func init() {
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbname := os.Getenv("MYSQL_DB_NAME")
 	user := os.Getenv("MYSQL_USERNAME")
 	pass := os.Getenv("MYSQL_PASSWORD")
-	dbname := os.Getenv("MYSQL_DB_NAME")
 	var err error
-	if DB, err = gorm.Open("mysql", fmt.Sprintf("%v:%v@/%v?charset=utf8&parseTime=True&loc=Local", user, pass, dbname)); err != nil {
+	if DB, err = gorm.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local", user, pass, host, port, dbname)); err != nil {
 		log.Fatal("failed to connect db: " + dbname)
 	}
 }
