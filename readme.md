3c697888a4e40bc95288a1959e83eee4226162a4
zouqilin <mr.zouqilin@gmail.com>
2017-11-28 11:22:27 +0800

    get registry config from env

--- readme.md
+++ readme.md
@@ -7,3 +7,27 @@ make [build_all|build_api|build_listing|build_order|build_user]
 ```bash
 make build TAG=v0.0.1 APP=[all|api|listing|order|user]
 ```
+
+### deploy
+1. mysql
+import tables to db
+  - `sql/db_listing.sql` to `db_listing`
+  - `sql/db_order.sql` to `db_order`
+  - `sql/db_user.sql` to `db_user`
+
+1. config
+```bash
+kubectl create -f k8s/config_map.yml
+```
+
+1. consul
+```bash
+kubectl create -f k8s/consul.yml
+```
+
+1. services
+```bash
+kubectl create -f k8s/user.yml
+kubectl create -f k8s/listing.yml
+kubectl create -f k8s/order.yml
+```
--- srv/user/service/init.go
+++ srv/user/service/init.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	// "micro_service_deployed_by_k8s//srv/user/model"
+	"fmt"
 	micro "github.com/micro/go-micro"
+	registry "github.com/micro/go-micro/registry"
 	"log"
 	proto "micro_service_deployed_by_k8s/srv/user/proto"
+	"os"
 )
 
 var (
@@ -22,7 +25,13 @@ func init() {
 		}),
 	)
 
-	service.Init()
+	registryServer := os.Getenv("REGISTRY_SERVER_HOST")
+	registryPort := os.Getenv("REGISTRY_SERVER_PORT")
+	registryAddr := fmt.Sprintf("%s:%s", server, registryPort)
+	registry := registry.NewRegistry(
+		registry.Addrs(registryAddr),
+	)
+	service.Init(micro.Registry(registry))
 
 	proto.RegisterServiceHandler(service.Server(), new(Service))
 }
